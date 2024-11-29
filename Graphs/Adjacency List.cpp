24657f84de49f6456828a67689a653aaffb42274
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2024-11-29 16:22:56 +0600

    Update Adjacency List.cpp

--- Graphs/Adjacency List.cpp
+++ Graphs/Adjacency List.cpp
@@ -2,32 +2,27 @@
 #include <vector>
 using namespace std;
 
-// Function to display adjacency list
-void printAdjacencyList(vector<vector<int>> &adj) 
+int main() 
 {
-    for (int a = 0; a < adj.size(); a++) 
+    int n, m;
+    cout<< "Nodes & Edges:";
+    cin >> n >> m;
+    vector<vector<int>> g(n);
+    for (int c = 0; c < m; c++) 
     {
-        cout << "Vertex " << a + 1 << " -> ";
-        for (int b : adj[a]) 
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+    }
+    cout<< "Node List\n";
+    for (int a = 0; a < n; a++) 
+    {
+        cout << a << ": ";
+        for (int v : g[a]) 
         {
-            cout << b + 1 << " "; // Display neighbors
+            cout << v << " ";
         }
         cout << endl;
     }
-}
-
-int main() {
-    int vertices = 3;
-
-    // Create an adjacency list
-    vector<vector<int>> adj(vertices);
-
-    // Add edges
-    adj[0].push_back(1); // Edge from 1 to 2
-    adj[0].push_back(2); // Edge from 1 to 3
-    adj[1].push_back(0); // Edge from 2 to 1
-    adj[2].push_back(1); // Edge from 3 to 2
-
-    printAdjacencyList(adj);
     return 0;
 }
