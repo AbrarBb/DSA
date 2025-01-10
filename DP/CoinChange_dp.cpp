57cfd6257d24dd374c114bad562d68703f6f87e9
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-01-11 03:48:39 +0600

    Update CoinChange_dp.cpp

--- DP/CoinChange_dp.cpp
+++ DP/CoinChange_dp.cpp
@@ -2,26 +2,25 @@
 #include <vector>
 using namespace std;
 
-int coinChange(vector<int>& coins, int amount) 
+int coinChange(vector<int>coins, int amount) 
 {
-    vector<int> dp(amount + 1, 1e9); 
+    vector<int> dp(amount + 1, 1e9); //1e9=infinity 
     dp[0] = 0; 
 
     for (int coin : coins) 
     {
-        for (int a = coin; a <= amount; ++a) 
+        for (int a = coin; a <= amount; a++) 
         {
             dp[a] = min(dp[a], dp[a - coin] + 1);
         }
     }
-
     return dp[amount] == 1e9 ? -1 : dp[amount];
 }
 
 int main() 
 {
-    vector<int> coins = {1, 2, 5};
-    int amount = 16;
+    vector<int> coins = {2,1,5};
+    int amount = 43;
     cout << coinChange(coins, amount) << endl; 
     return 0;
 }
