4f2a025199e79a5127610da910cc52dcdc9a0a11
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-01-11 04:01:24 +0600

    Update CoinChange2_dp.cpp

--- DP/CoinChange2_dp.cpp
+++ DP/CoinChange2_dp.cpp
@@ -2,41 +2,36 @@
 #include <vector>
 using namespace std;
 
-void coinChange(vector<int>& coins, int amount) 
+int coinChange(vector<int>coins, int amount) 
 {
-    vector<int> dp(amount + 1, 1e9), lastCoin(amount + 1, -1);
-    dp[0] = 0;
+    vector<int> dp(amount + 1, 1e9); //1e9=infinity 
+    vector<int> lastCoin(amount + 1, -1);
+    dp[0] = 0; 
 
     for (int coin : coins) 
     {
-        for (int i = coin; i <= amount; ++i) 
+        for (int a = coin; a <= amount; a++) 
         {
-            if (dp[i - coin] + 1 < dp[i]) 
-            {
-                dp[i] = dp[i - coin] + 1;
-                lastCoin[i] = coin;
-            }
+            dp[a] = min(dp[a], dp[a - coin] + 1);
+            lastCoin[a] = coin;
         }
     }
-
-    if (dp[amount] == 1e9) 
+    
+    
+cout << "Coins needed: " << dp[amount] << "\nCoins used: ";
+    for (int a = amount; a > 0; a -= lastCoin[a]) 
     {
-        cout << "Not possible to make the amount.\n";
-        return;
+        cout << lastCoin[a] << " ";
     }
-
-    cout << "Minimum coins needed: " << dp[amount] << "\nCoins used: ";
-    for (int i = amount; i > 0; i -= lastCoin[i]) 
-    {
-        cout << lastCoin[i] << " ";
-    }
-    cout << endl;
+    
+    return dp[amount] == 1e9 ? -1 : dp[amount];
 }
 
 int main() 
 {
-    vector<int> coins = {1, 2, 5};
-    int amount = 11;
+    vector<int> coins = {2,1,5};
+    int amount = 43;
     coinChange(coins, amount);
+    //cout << coinChange(coins, amount) << endl; 
     return 0;
 }
