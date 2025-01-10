e4d595d50acd70e5e71d03c41659c9a3a1145c64
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-01-11 04:23:04 +0600

    Update LCS_dp.cpp

--- DP/LCS_dp.cpp
+++ DP/LCS_dp.cpp
@@ -1,34 +1,27 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int LCS(string s1, string s2) 
 {
     int m = s1.size(), n = s2.size();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-    // Build the DP table
-    for (int i = 1; i <= m; ++i) 
+    vector<vector<int>> dp(m+1, vector<int>(n+1));
+    
+    for(int i = 1; i <= m; i++) 
     {
-        for (int j = 1; j <= n; ++j) 
+        for(int j = 1; j <= n; j++) 
         {
-            if (s1[i - 1] == s2[j - 1]) 
-            {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } 
+            if(s1[i-1] == s2[j-1]) 
+                dp[i][j] = 1 + dp[i-1][j-1];
             else 
-            {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
-
-    return dp[m][n]; // Return the length of the LCS
+    return dp[m][n];
 }
 
-int main() 
-{
-    string s1 = "ABCBDAB", s2 = "BDCABB";
-    cout << "Length of LCS: " << LCS(s1, s2) << endl; // Output: 4
+int main() {
+    string s1 = "STONE";
+    string s2 = "LONGEST";
+    cout << LCS(s1, s2);  // prints 3 (LCS is "ONE")
     return 0;
 }
